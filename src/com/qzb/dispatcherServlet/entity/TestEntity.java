0aca5e622ba3b26b10bd05db5e714da7e1bc652f
qzbing <qzb0212@126.com>
2019-05-26 14:18:36 +0800

    git commit

--- src/com/qzb/dispatcherServlet/entity/TestEntity.java
+++ src/com/qzb/dispatcherServlet/entity/TestEntity.java
@@ -1,4 +1,81 @@
 package com.qzb.dispatcherServlet.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class TestEntity {
+
+    private Long id;
+    private String message;
+    private Date time;
+    private Double latitude;
+    private Double longitude;
+
+    public TestEntity() {
+    }
+
+    public TestEntity(Long id, String message, Date time, Double latitude, Double longitude) {
+        this.id = id;
+        this.message = message;
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TestEntity)) return false;
+        TestEntity that = (TestEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(message, that.message) &&
+                Objects.equals(time, that.time) &&
+                Objects.equals(latitude, that.latitude) &&
+                Objects.equals(longitude, that.longitude);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, message, time, latitude, longitude);
+    }
 }
